a4e9569f00d7cc56b51130978ead346c1a512e2d
mfaydin49 <mfaydin49@gmail.com>
2020-06-25 22:16:22 +0200

    edited

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -15,6 +15,7 @@ class Business extends React.Component {
             <p>{this.props.business.address}</p>
             <p>{this.props.business.state} {this.props.business.zipCode}</p>
             <p>{this.props.business.phone}</p>
+            <p>{this.props.business.phone}</p>
           </div>
           <div className="Business-reviews">
             <h3>{this.props.business.category}</h3>
